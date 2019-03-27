0c522e53504431bd97ed3a8152d5a6e0a337feba
TuKDoan <k.doan@utexas.edu>
2019-03-27 01:46:22 -0500

    create 2D grid and populate with 1s and 0s

--- sketch.js
+++ sketch.js
@@ -1,7 +1,20 @@
-function setup() {
+let grid;
+let cols = 10;
+let rows = 10;
 
+function make2DArray(cols, rows){
+  let arr = new Array(cols);
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = new Array(rows);
+  }
+  return arr;
 }
 
-function draw() {
-  ellipse(50, 50, 80, 80);
+function setup() {
+  grid = make2DArray(cols, rows);
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j] = floor(random(2));
+    }
+  }
 }
