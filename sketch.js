8ddee0d0c0f699fdab5728459819da9f8ef23d7e
TuKDoan <k.doan@utexas.edu>
2019-03-27 01:53:50 -0500

    render grid with black and whitesquares

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
 let grid;
-let cols = 10;
-let rows = 10;
+let cols
+let rows;
+let reso = 40;
+let canvx = 1000;
+let canvy = 1000;
 
 function make2DArray(cols, rows){
   let arr = new Array(cols);
@@ -11,10 +14,28 @@ function make2DArray(cols, rows){
 }
 
 function setup() {
+  createCanvas (canvx, canvy);
+  cols = width / reso;
+  rows = width / reso;
+
   grid = make2DArray(cols, rows);
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
       grid[i][j] = floor(random(2));
+    }
+  }
+}
+
+function draw() {
+  background(0);
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let x = i * reso;
+      let y = j * reso;
+      if (grid[i][j] == 1) {
+        fill(255);
+        rect(x, y, reso-1, reso-1);
+      }
     }
   }
 }
